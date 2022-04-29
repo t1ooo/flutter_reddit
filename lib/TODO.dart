1b007562e5c18c4381151d6affeeb04d9a44a261
graibn <5966878+t1ooo@users.noreply.github.com>
2022-04-29 15:52:41 +0500

    update todo

--- lib/TODO.dart
+++ lib/TODO.dart
@@ -1,7 +1,4 @@
-// TODO: image cache
-// TODO: remove context from widget helpers
-// TODO: flutter image.network catch exception
-// TODO: login on voit, ets
+// TODO: BUG: submission page not loaded
 // TODO: refresh
 // TODO: merge loggers
 // TODO: listview save croll position
@@ -9,7 +6,7 @@
 // TODO: load on screen on widget ?
 // TODO: add top padding to page padding
 // TODO: save/unsave, vote, share interfaces for generic widget implementation
-// TODO: use controller instead watch
+// TODO: MEAYBE: use controller instead watch
 // TODO: retry, undo, reload
 // TODO: user inbox
 // TODO: login
@@ -18,27 +15,13 @@
 // TODO: block user
 // TODO: report
 // TODO: hide/unhide submission
-// TODO: cache
 // TODO: remove non-displayed fields from reddit_api models
 // TODO: subreddit notification
 // TODO: gestures
 // TODO: make padding consistent
-// TODO: subreddit type
 // TODO: design
 // TODO: trendings (not supported by Reddit API)
 // TODO: user chat (not supported by Reddit API)
 // TODO: give award to submission (not supported by DRAW lib)
 // TODO: give award to comment (not supported by DRAW lib)
 // TODO: filter subscriptions by location (not supported by Reddit API)
-//  v1
-//  class SubmissionNotifier
-//  class SubmissionTileNotifier
-//  class CommentNotifier
-//  class CommentTileNotifier
-//  class CurrentUserNotifier
-//  class UserNotifier
-//  v2
-//  class SubmissionNotifier
-//  class SubmissionsNotifier
-//  class CommentsNotifier
-
