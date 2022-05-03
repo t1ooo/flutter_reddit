4fab821d26c52ae49063eb94788a8846aa7d6bcf
graibn <5966878+t1ooo@users.noreply.github.com>
2022-05-04 00:17:55 +0500

    add generateColor

--- lib/src/util/color.dart
+++ lib/src/util/color.dart
@@ -1,9 +1,22 @@
+import 'dart:math';
+
 import 'package:flutter/widgets.dart';
 
 Color? colorFromHex(String hex) {
+  if (hex == '') {
+    return null;
+  }
   final value = int.tryParse('FF' + hex.replaceFirst('#', ''), radix: 16);
   if (value != null) {
     return Color(value);
   }
   return null;
+}
+
+/// return color generated from string
+Color generateColor(String s) {
+  const min = 0;
+  const max = 0xFFFFFFFF;
+  final value = min + Random(s.hashCode).nextInt(max - min);
+  return Color(value);
 }
