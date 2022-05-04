89d2e198f21be7240f642c39dc897699c3204d92
graibn <5966878+t1ooo@users.noreply.github.com>
2022-05-04 12:45:33 +0500

    rewrite user info

--- lib/src/user_profile/user_info.dart
+++ lib/src/user_profile/user_info.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:provider/provider.dart';
 
 import '../notifier/reddir_notifier.v4_2.dart';
@@ -15,61 +19,115 @@ class UserInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final notifier = context.read<UserNotifierQ>();
+    final subreddit = notifier.subreddit.subreddit;
     final user = notifier.user;
 
     return Container(
       color: Theme.of(context).primaryColor,
-      padding: pagePadding,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(height: topPadding),
-          Row(
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    width: iconSize,
-                    height: iconSize,
-                    // child: Image.network(user.iconImg),
-                    child: CustomNetworkImageBuilder(user.iconImg),
-                  ),
-                ],
-              ),
-              SizedBox(width: 10),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(user.name, textScaleFactor: 2),
-                  Text(user.subreddit.displayNamePrefixed),
-                ],
-              ),
-            ],
-          ),
-          SizedBox(height: 20),
-          if (notifier.isCurrentUser)
-            ElevatedButton(onPressed: () {}, child: Text('EDIT'))
-          else
+      child: Padding(
+        padding: pagePadding,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
             Row(
-              // alignment: MainAxisAlignment.start,
               children: [
-                ElevatedButton(onPressed: () {}, child: Text('CHAT')),
+                SizedBox(height: 100),
+                if (user.iconImg != '')
+                  CircleAvatar(
+                    radius: 16, // Image radius
+                    foregroundImage: CachedNetworkImageProvider(
+                      user.iconImg,
+                      cacheManager: context.read<CacheManager>(),
+                    ),
+                    onForegroundImageError: (e, _) => log('$e'),
+                  ),
+                // CachedNetworkImage(
+                //   imageUrl: subreddit.communityIcon,
+                //   fit: BoxFit.fitHeight,
+                //   errorWidget: (_, __, error) {
+                //     print(error);
+                //     return Container();
+                //   },
+                //   imageBuilder: (context, imageProvider) => CircleAvatar(
+                //     radius: 16, // Image radius
+                //     backgroundImage: imageProvider,
+                //   ),
+                // ),
                 SizedBox(width: 10),
+                Text(subreddit.displayNamePrefixed, textScaleFactor: 2),
+                Spacer(),
+                // ElevatedButton(onPressed: () {}, child: Icon(Icons.doorbell)),
+                // SizedBox(width: 10),
                 ChangeNotifierProvider<SubredditNotifierQ>.value(
                   value: notifier.subreddit,
-                  child: SubscribeButton(isUserPage: true),
+                  child: SubscribeButton(),
                 ),
               ],
             ),
-          SizedBox(height: 20),
-          Text(
+            // SizedBox(height: 10),
+            Text(
               '${user.totalKarma} karma • ${formatDateTime(user.created)} • ${'${user.subreddit.subscribers} followers'}'),
-          SizedBox(height: 10),
-          Text(user.subreddit.publicDescription),
-          SizedBox(height: 20),
-        ],
+            SizedBox(height: 20),
+            Text(subreddit.publicDescription),
+            SizedBox(height: 50),
+          ],
+        ),
       ),
     );
+
+    // return Container(
+    //   color: Theme.of(context).primaryColor,
+    //   padding: pagePadding,
+    //   child: Column(
+    //     crossAxisAlignment: CrossAxisAlignment.start,
+    //     children: [
+    //       SizedBox(height: topPadding),
+    //       Row(
+    //         children: [
+    //           Column(
+    //             crossAxisAlignment: CrossAxisAlignment.start,
+    //             children: [
+    //               SizedBox(
+    //                 width: iconSize,
+    //                 height: iconSize,
+    //                 // child: Image.network(user.iconImg),
+    //                 child: CustomNetworkImageBuilder(user.iconImg),
+    //               ),
+    //             ],
+    //           ),
+    //           SizedBox(width: 10),
+    //           Column(
+    //             crossAxisAlignment: CrossAxisAlignment.start,
+    //             children: [
+    //               Text(user.name, textScaleFactor: 2),
+    //               Text(user.subreddit.displayNamePrefixed),
+    //             ],
+    //           ),
+    //         ],
+    //       ),
+    //       SizedBox(height: 20),
+    //       if (notifier.isCurrentUser)
+    //         ElevatedButton(onPressed: () {}, child: Text('EDIT'))
+    //       else
+    //         Row(
+    //           // alignment: MainAxisAlignment.start,
+    //           children: [
+    //             ElevatedButton(onPressed: () {}, child: Text('CHAT')),
+    //             SizedBox(width: 10),
+    //             ChangeNotifierProvider<SubredditNotifierQ>.value(
+    //               value: notifier.subreddit,
+    //               child: SubscribeButton(isUserPage: true),
+    //             ),
+    //           ],
+    //         ),
+    //       SizedBox(height: 20),
+    //       Text(
+    //           '${user.totalKarma} karma • ${formatDateTime(user.created)} • ${'${user.subreddit.subscribers} followers'}'),
+    //       SizedBox(height: 10),
+    //       Text(user.subreddit.publicDescription),
+    //       SizedBox(height: 20),
+    //     ],
+    //   ),
+    // );
   }
 }
