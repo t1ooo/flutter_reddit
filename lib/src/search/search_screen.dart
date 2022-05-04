7e2f77148c9cb0fc07c943a669a89a63c41da763
graibn <5966878+t1ooo@users.noreply.github.com>
2022-05-04 16:43:46 +0500

    refactor SearchScreen

--- lib/src/search/search_screen.dart
+++ lib/src/search/search_screen.dart
@@ -1,11 +1,44 @@
 import 'package:flutter/material.dart';
 
 import '../style/style.dart';
+import '../widget/sliver_app_bar.dart';
+import '../widget/space_bar.dart';
 import 'search.dart';
-import 'search_field.dart';
+// import 'search_field.dart';
+
+// class SearchScreen extends StatelessWidget {
+//   const SearchScreen({
+//     Key? key,
+//     required this.query,
+//   }) : super(key: key);
+
+//   final String query;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return DefaultTabController(
+//       length: 2,
+//       child: Scaffold(
+//         appBar: AppBar(
+//           title: Text('SearchScreen'),
+//         ),
+//         body: Column(
+//           children: [
+//             SizedBox(height: topPadding),
+//             Padding(
+//               padding: pagePadding,
+//               child: SearchField(query: query),
+//             ),
+//             Expanded(child: Search(query: query)),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 class SearchScreen extends StatelessWidget {
-  const SearchScreen({
+  SearchScreen({
     Key? key,
     required this.query,
   }) : super(key: key);
@@ -14,22 +47,22 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('SearchScreen'),
-        ),
-        body: Column(
-          children: [
-            SizedBox(height: topPadding),
-            Padding(
-              padding: pagePadding,
-              child: SearchField(query: query),
+    return Scaffold(
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) {
+          print(innerBoxIsScrolled);
+          return [
+            PrimarySliverAppBar(
+              flexibleSpace: SpaceBar(
+                leading: SearchBackButton(),
+                // leading: SearchBackButton.black(),
+                title: SearchForm(query: query),
+              ),
             ),
-            Expanded(child: Search(query: query)),
-          ],
-        ),
+          ];
+        },
+        // body: Expanded(child: Search(query: query)),
+        body: Search(query: query),
       ),
     );
   }
