61d102f24d7b88286bfdc0b5d018f1b92693fbde
graibn <5966878+t1ooo@users.noreply.github.com>
2022-05-04 16:41:48 +0500

    update style

--- lib/src/style/style.dart
+++ lib/src/style/style.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+const black = Colors.black87;
 // const scrollPadding = EdgeInsets.all(0);
 // const pagePadding = EdgeInsets.all(20);
 const pagePadding = EdgeInsets.symmetric(horizontal: 20);
@@ -9,7 +10,7 @@ const topPadding = 20.0;
 const iconSize = 50.0;
 const selectedColor = Colors.blue;
 // const appBarIconTheme = IconThemeData(color: Colors.white, size: 30);
-const appBarIconTheme = IconThemeData(color: Colors.white, size: 30);
+// const appBarIconTheme = IconThemeData(color: Colors.white, size: 30);
 const appBarIconThemeDark = IconThemeData(color: black, size: 30);
-const black = Colors.black87;
+const formIconTheme = IconThemeData(color: Colors.black54);
 const appBarExpandedHeight = 200.0;
