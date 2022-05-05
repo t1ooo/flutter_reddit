ee8f42a18e616ac3b4bb9930d1f48f4857164b1a
graibn <5966878+t1ooo@users.noreply.github.com>
2022-05-05 12:37:47 +0500

    update theme

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
         appBarTheme: AppBarTheme(
           iconTheme: appBarIconThemeDark,
           elevation: 0,
-          toolbarHeight: 150,
+          // toolbarHeight: 150,
           // foregroundColor: Colors.white,
         ),
         snackBarTheme: SnackBarThemeData(
