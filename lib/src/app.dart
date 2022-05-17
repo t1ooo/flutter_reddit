6895f74f33cded91373d71fa30aed9c79720995a
graibn <5966878+t1ooo@users.noreply.github.com>
2022-05-17 22:28:15 +0500

    chane app title

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Reddit App',
       theme: theme,
       home: CustomScroll(
         child: StartScreen(),
