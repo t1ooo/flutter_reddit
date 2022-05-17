ca68f11c64ac74c5568b5d3f44c14958c3897083
graibn <5966878+t1ooo@users.noreply.github.com>
2022-05-17 22:27:54 +0500

    move style

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -11,50 +11,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primaryColor: Colors.white,
-        primarySwatch: white,
-        textButtonTheme: TextButtonThemeData(
-          style: TextButton.styleFrom(
-            primary: blackColor,
-            textStyle: TextStyle(
-              fontWeight: FontWeight.normal,
-            ),
-          ),
-        ),
-        radioTheme: RadioThemeData(
-          fillColor: MaterialStateProperty.all(blackColor),
-          overlayColor: MaterialStateProperty.all(blackColor),
-        ),
-        iconTheme: IconThemeData(color: blackColor),
-        appBarTheme: AppBarTheme(
-          iconTheme: appBarIconThemeDark,
-          elevation: 0,
-        ),
-        snackBarTheme: SnackBarThemeData(
-          backgroundColor: Colors.white,
-          contentTextStyle: TextStyle(color: blackColor),
-          behavior: SnackBarBehavior.floating,
-        ),
-        indicatorColor: selectedColor,
-      ),
+      theme: theme,
       home: CustomScroll(
         child: StartScreen(),
       ),
     );
   }
 }
-
-final MaterialColor white =
-    MaterialColor(Colors.white.value, const <int, Color>{
-  50: Colors.white,
-  100: Colors.white,
-  200: Colors.white,
-  300: Colors.white,
-  400: Colors.white,
-  500: Colors.white,
-  600: Colors.white,
-  700: Colors.white,
-  800: Colors.white,
-  900: Colors.white,
-});
--- lib/src/style/style.dart
+++ lib/src/style/style.dart
@@ -10,3 +10,44 @@ const appBarIconThemeDark = IconThemeData(color: blackColor, size: 30);
 const formIconTheme = IconThemeData(color: Colors.black54);
 const appBarCollapsedHeight = 120.0;
 const appBarExpandedHeight = 200.0;
+
+final white = MaterialColor(Colors.white.value, const <int, Color>{
+  50: Colors.white,
+  100: Colors.white,
+  200: Colors.white,
+  300: Colors.white,
+  400: Colors.white,
+  500: Colors.white,
+  600: Colors.white,
+  700: Colors.white,
+  800: Colors.white,
+  900: Colors.white,
+});
+
+final theme = ThemeData(
+  primaryColor: Colors.white,
+  primarySwatch: white,
+  textButtonTheme: TextButtonThemeData(
+    style: TextButton.styleFrom(
+      primary: blackColor,
+      textStyle: TextStyle(
+        fontWeight: FontWeight.normal,
+      ),
+    ),
+  ),
+  radioTheme: RadioThemeData(
+    fillColor: MaterialStateProperty.all(blackColor),
+    overlayColor: MaterialStateProperty.all(blackColor),
+  ),
+  iconTheme: IconThemeData(color: blackColor),
+  appBarTheme: AppBarTheme(
+    iconTheme: appBarIconThemeDark,
+    elevation: 0,
+  ),
+  snackBarTheme: SnackBarThemeData(
+    backgroundColor: Colors.white,
+    contentTextStyle: TextStyle(color: blackColor),
+    behavior: SnackBarBehavior.floating,
+  ),
+  indicatorColor: selectedColor,
+);
