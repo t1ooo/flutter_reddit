93b22fef536af9c6b25c2ee395f0954927d9b28d
graibn <5966878+t1ooo@users.noreply.github.com>
2022-05-18 22:18:24 +0500

    update todo

--- lib/TODO.dart
+++ lib/TODO.dart
@@ -1,3 +1,4 @@
+// TODO: add dart analytics options
 // TODO: optimistic updates
 // TODO: auto disabled buttons
 // TODO: remove unused containers
@@ -6,7 +7,7 @@
 // TODO: use catchFutureError
 // TODO: remove unused code
 // TODO: split notifiers to files
-// TODO: access to fields via notifier
+// TODO: access to fields via notifier, make models mutable
 
 // TODO: show comment link_url image
 // TODO: submission tile show domain
@@ -15,19 +16,14 @@
 // TODO: inbox mod message
 // TODO: inbox message reply
 // TODO: link|image|video post submitting
-// TODO: MAYBE: wrap all model field wit notifier getter, make models mutable
-// TODO: configure app bar elevation
 // TODO: ((un)subscrube (un)save reply post) update user saved subscribed
 //      TODO?: move actions to CurrentUserNotifier
 //      TODO?: cache to small time CurrentUserNotifier saved and subscribed
 //      NOTE: reddit app use manual refresh
 // TODO: hide bottom bar on some screens
-// TODO: explicit wrap each widget with provider
-// TODO: load on screen on widget ?
 // TODO: add top padding to page padding
 // TODO: share interfaces for generic widget implementation
-// TODO: MEAYBE: use controller instead watch
-// TODO: retry, undo, reload
+// TODO: retry, undo
 // TODO: user inbox
 // TODO: show/edit current user settings
 // TODO: show/edit current user profile
@@ -35,10 +31,7 @@
 // TODO: report
 // TODO: hide/unhide submission
 // TODO: remove non-displayed fields from reddit_api models
-// TODO: gestures
 // TODO: make padding consistent
-// TODO: design
-// TODO: subreddit notification
 // TODO: trendings (not supported by Reddit API)
 // TODO: user chat (not supported by Reddit API)
 // TODO: give award to submission (not supported by DRAW lib)
