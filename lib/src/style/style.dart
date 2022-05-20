1d8f973e76b74026e018b99249791cf5417d049c
graibn <5966878+t1ooo@users.noreply.github.com>
2022-05-20 13:46:28 +0500

    update style

--- lib/src/style/style.dart
+++ lib/src/style/style.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 const blackColor = Colors.black87;
 const pagePadding = EdgeInsets.symmetric(horizontal: 20);
-const cardPadding = EdgeInsets.only(top: 10, bottom: 20, left: 20, right: 20);
+const cardPadding = EdgeInsets.only(top: 20, bottom: 20, left: 20, right: 20);
 const topPadding = 20.0;
 const iconSize = 50.0;
 const selectedColor = Colors.blue;
