6bd84b335e307196bd4bd344f6f89034f4bd3544
graibn <5966878+t1ooo@users.noreply.github.com>
2022-05-24 13:48:41 +0500

    fix run scripts

--- run_android.sh
+++ run_android.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 source "./.env.mobile"
-# export REDDIT_REDIRECT_URI="$REDDIT_REDIRECT_URI" # gradle env
-export REDDIT_REDIRECT_URI="" # gradle env
+export REDDIT_REDIRECT_URI="$REDDIT_REDIRECT_URI" # gradle env
 flutter run \
-    --dart-define=REDDIT_CLIENT_ID="$(printf "%q" "$REDDIT_CLIENT_ID")" \
-    --dart-define=REDDIT_REDIRECT_URI="$(printf "%q" "$REDDIT_REDIRECT_URI")" \
+    --dart-define="REDDIT_CLIENT_ID=$REDDIT_CLIENT_ID" \
+    --dart-define="REDDIT_REDIRECT_URI=$REDDIT_REDIRECT_URI" \
     -d android "$@"
--- run_linux.sh
+++ run_linux.sh
@@ -2,6 +2,6 @@
 
 source "./.env.desktop"
 flutter run \
-    --dart-define=REDDIT_CLIENT_ID="$(printf "%q" "$REDDIT_CLIENT_ID")" \
-    --dart-define=REDDIT_REDIRECT_URI="$(printf "%q" "$REDDIT_REDIRECT_URI")" \
+    --dart-define="REDDIT_CLIENT_ID=$REDDIT_CLIENT_ID" \
+    --dart-define="REDDIT_REDIRECT_URI=$REDDIT_REDIRECT_URI" \
     -d linux "$@"
