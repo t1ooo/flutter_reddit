330e8beb8f83ed0740bb41250ea358c8fa6c79d4
graibn <5966878+t1ooo@users.noreply.github.com>
2022-05-29 13:12:47 +0500

    add AsyncButtonBuilderBehavior

--- lib/src/widget/async_button_builder.dart
+++ lib/src/widget/async_button_builder.dart
@@ -1,33 +1,56 @@
 import 'package:flutter/material.dart';
 
+enum AsyncButtonBuilderBehavior {
+  /// ignore click when onPressed is executed
+  ignore,
+
+  /// disable button when onPressed is executed
+  disable,
+}
+
 typedef FutureVoidCallback = Future<void> Function();
 
-/// pass onPressed argument to the builder or null when onPressed is executed
+/// pass onPressed argument to the builder or (empty callback|null) when onPressed is executed
 class AsyncButtonBuilder extends StatefulWidget {
   const AsyncButtonBuilder({
     Key? key,
     required this.onPressed,
     required this.builder,
+    this.behavior = AsyncButtonBuilderBehavior.ignore,
   }) : super(key: key);
 
   final FutureVoidCallback onPressed;
   final Widget Function(BuildContext, FutureVoidCallback?) builder;
+  final AsyncButtonBuilderBehavior behavior;
 
   @override
   State<AsyncButtonBuilder> createState() => _AsyncButtonBuilderState();
 }
 
 class _AsyncButtonBuilderState extends State<AsyncButtonBuilder> {
-  bool _disabled = false;
+  bool _enabled = true;
 
   @override
   Widget build(BuildContext context) {
-    return widget.builder(context, _disabled ? null : _onPressed);
+    return widget.builder(context, _fnOnPressed());
   }
 
+  FutureVoidCallback? _fnOnPressed() {
+    if (_enabled) return _onPressed;
+
+    switch (widget.behavior) {
+      case AsyncButtonBuilderBehavior.ignore:
+        return _onPressedIgnore;
+      case AsyncButtonBuilderBehavior.disable:
+        return null;
+    }
+  }
+
+  Future<void> _onPressedIgnore() async {/* do nothing */}
+
   Future<void> _onPressed() async {
-    setState(() => _disabled = true);
+    setState(() => _enabled = false);
     await widget.onPressed();
-    setState(() => _disabled = false);
+    setState(() => _enabled = true);
   }
 }
